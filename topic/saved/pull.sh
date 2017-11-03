6478c8127180bbc66a91a62b91963b23514947c3
THUcqb <chenqibin422@gmail.com>
2017-11-03 22:36:24 +0800

    feat: add basic document class and topic folder.

---
+++ topic/saved/pull.sh
@@ -0,0 +1,4 @@
+wget https://www.dropbox.com/s/hiwnltipvl1xnyq/word2vec_model
+wget https://www.dropbox.com/s/9gzm12pfue3qrtx/word2vec_model.syn1neg.npy
+wget https://www.dropbox.com/s/y6uj1sdlvwa7d2k/word2vec_model.wv.syn0.npy
+wget https://www.dropbox.com/s/cm3p3xxrsjwsyl1/lyrics.pkl
