8fbe668a75e95c97f122e9c0a13c194a5f632e38
THUcqb <chenqibin422@gmail.com>
2017-11-12 18:47:25 +0800

    docs: add theme and move to /docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,6 +21,10 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 
 # -- General configuration ------------------------------------------------
 
