7aff756b3beec77bd2a6a7396f58fcb31f1f2eda
THUcqb <chenqibin422@gmail.com>
2017-11-30 23:54:39 +0800

    feat: add beam_search from magenta.
    Still have some problems. Not sure why
    
    Relate #8

--- models/word_language_model/generate.py
+++ models/word_language_model/generate.py
@@ -4,13 +4,15 @@
 # This file generates new sentences sampled from the language model
 #
 ###############################################################################
-
+# %%
 import argparse
 
+import numpy as np
 import torch
 from torch.autograd import Variable
 
 import data
+from models.beam_search import beam_search
 
 parser = argparse.ArgumentParser(description='PyTorch PTB Language Model')
 
@@ -55,20 +57,54 @@ else:
 
 corpus = data.Corpus(args.data)
 ntokens = len(corpus.dictionary)
+
+# %%
 hidden = model.init_hidden(1)
 input = Variable(torch.rand(1, 1).mul(ntokens).long(), volatile=True)
 if args.cuda:
     input.data = input.data.cuda()
 
-with open(args.outf, 'w') as outf:
-    for i in range(args.words):
-        output, hidden = model(input, hidden)
+def greedy_decode(input, hidden):
+    with open(args.outf, 'w') as outf:
+        for i in range(args.words):
+            output, hidden = model(input, hidden)
+            word_weights = output.squeeze().data.div(args.temperature).exp().cpu()
+            word_idx = torch.multinomial(word_weights, 1)[0]
+            input.data.fill_(word_idx)
+            word = corpus.dictionary.idx2word[word_idx]
+
+            outf.write(word + ' ')
+
+            if i % args.log_interval == 0:
+                print('| Generated {}/{} words'.format(i, args.words))
+
+def generate_step_fn(sequences, states, scores):
+    new_sequences, new_states, new_scores = [], [], []
+    for sequence, state, score in zip(sequences, states, scores):
+        output, state = model(sequence[-1].view(-1, 1), state)
         word_weights = output.squeeze().data.div(args.temperature).exp().cpu()
         word_idx = torch.multinomial(word_weights, 1)[0]
-        input.data.fill_(word_idx)
-        word = corpus.dictionary.idx2word[word_idx]
 
-        outf.write(word + ' ')
+        new_token = Variable(torch.LongTensor([word_idx]).view(-1, 1))
+        if args.cuda:
+            new_token.data = new_token.data.cuda()
+        new_sequences.append(torch.cat([sequence, new_token]))
+        new_states.append(state)
+        new_scores.append(score + np.log(word_weights[word_idx] / torch.sum(word_weights)))
 
-        if i % args.log_interval == 0:
-            print('| Generated {}/{} words'.format(i, args.words))
+    return new_sequences, new_states, new_scores
+
+def beam_decode(input, hidden, num_steps=50, beam_size=10, branch_factor=10, steps_per_iteration=1):
+    with open(args.outf, 'w') as outf:
+        for i in range(len(input)- 1):
+            print(i)
+            _, hidden = model(input[i].view(-1, 1), hidden)
+        selected = beam_search(input, hidden, generate_step_fn, num_steps,\
+                beam_size, branch_factor, steps_per_iteration)
+        sequence, state, score = selected
+        print(sequence.data)
+        for word_idx in sequence.data:
+            word = corpus.dictionary.idx2word[word_idx[0]]
+            outf.write(word + ' ')
+
+beam_decode(input, hidden)
